f14e3b5284f0808c0e129592df3878190950b25d
DanielPodbury <82479887+DanielPodbury@users.noreply.github.com>
2022-05-10 14:35:00 +1000

    Continous integration (#4)
    Co-authored-by: Dan Podbury <dan.podbury@gmail.com>

--- A2/starter_code/tests/runTests.bash
+++ A2/starter_code/tests/runTests.bash
@@ -2,6 +2,7 @@
 # NOTE : Run this script from the /tests/ directory, or it won't work
 
 FILES="./tests/*.input"
+FAILS=0
 
 if [ -e "./scrabble"  ] 
 then
@@ -24,6 +25,7 @@ then
             echo -e "\e[32;5m$E PASSED!\e[0m"
         else
             echo -e "\e[31m$E FAILED!\e[0m"
+            ((FAILS=FAILS+1))
             #echo "!     Differences: ${#differences}     !"
             diff -w --color $E.output $E.gameout
             
@@ -33,8 +35,12 @@ then
         # take action on each file. $f store current file name
         #cat "$f"
     done
-    
+
 else
     echo -e "\e[31mTests FAILED to run!\e[0m"
     echo "Scrabble.exe not found"
+    ((FAILS=1))
 fi
+
+echo $FAILS
+exit $FAILS
